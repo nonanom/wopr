9ef73e00fe3a1c71f9d4ac705bea2b658e1f6d6d
Michael Vincent <michael.vincent@cubic.com>
2021-02-21 17:49:58 -0800

    pretty format

--- init.sql
+++ init.sql
@@ -1,7 +1,3 @@
-CREATE TABLE message (
-    id      integer,
-    text    text
-);
-
+CREATE TABLE message (id integer, text text);
 INSERT INTO message (id, text)
 VALUES(1, 'This message comes from a Postgres database.');
